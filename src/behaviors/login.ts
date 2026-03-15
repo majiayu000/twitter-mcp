8cd85fe2f6d87a947e1b7bc14b6ad35757da3066
majiayu000 <1835304752@qq.com>
2026-03-15 15:17:37 +0800

    fix: 代理密码脱敏、空catch块补错误日志、SIGINT改为once

--- src/behaviors/login.ts
+++ src/behaviors/login.ts
@@ -154,7 +154,7 @@ function getProxyConfig() {
     }
   }
 
-  console.log("Using proxy config:", proxyConfig);
+  console.log("Using proxy config:", { ...proxyConfig, password: proxyConfig.password ? "***" : undefined });
   return {
     server: proxyConfig.server,
     ...(proxyConfig.username && { username: proxyConfig.username }),
